f231e84400b23dff1cfb5161c2ca5441f6e2621d
Bakare Shukuralillahi <bakareshukurah@outlook.com>
2022-02-15 05:44:07 +0100

    Included  meal items and form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 
 const MealItem = props => {
@@ -10,6 +11,9 @@ const MealItem = props => {
                 <h3>{props.name}</h3>
                 <div className={styles.description}>{props.description}</div>
                 <div className={styles.price}>{price}</div>
+            </div>
+            <div>
+                <MealItemForm id={props.id}/>
             </div>
         </li>
     )
